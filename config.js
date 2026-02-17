12eb27a8de567c5540792c4bb5546bafe36aeb78
TeritoriirRromani <teritorii.rromani@gmail.com>
2026-02-17 22:56:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
-// config.js — UN SINGUR LOC pentru URL-ul Apps Script
+/* =========================================================
+   CONFIG GLOBAL APLICAȚIE
+   ---------------------------------------------------------
+   Aici modifici URL-ul Web App-ului Google Apps Script.
+   Dacă redeployezi scriptul, schimbi DOAR aici.
+   ========================================================= */
+
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwJoRjlNGMAx0_WbHD19o2_dyFiH1fsyqfs8r8shLkrNMc-AP-KmMDuTp449gp1fe2_/exec";
