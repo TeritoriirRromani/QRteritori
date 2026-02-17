0f4334d3987aaf6604c179b56c4ca6ec518ff1c9
TeritoriirRromani <teritorii.rromani@gmail.com>
2026-02-17 23:04:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
    Dacă redeployezi scriptul, schimbi DOAR aici.
    ========================================================= */
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwJoRjlNGMAx0_WbHD19o2_dyFiH1fsyqfs8r8shLkrNMc-AP-KmMDuTp449gp1fe2_/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbypMhUM4A_jg18aqGCV-C_TSNe8BRl76DCgDpx6yVfauh_WrwsLJXyUmF8kLBhKiZjD/exec";
