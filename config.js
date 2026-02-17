1a28ce3299a7710780db13df40c3314ad3ead9b3
TeritoriirRromani <teritorii.rromani@gmail.com>
2026-02-17 22:56:01 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+// config.js — UN SINGUR LOC pentru URL-ul Apps Script
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwJoRjlNGMAx0_WbHD19o2_dyFiH1fsyqfs8r8shLkrNMc-AP-KmMDuTp449gp1fe2_/exec";
